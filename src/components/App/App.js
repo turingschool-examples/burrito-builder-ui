647c7d76284a31323b81b432843ff671718e374b
Shauna Myers <shaunamyers26@gmail.com>
2021-08-10 16:25:17 +0100

    Log orders to check fetch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,6 +14,7 @@ class App extends Component {
 
   componentDidMount() {
     getOrders()
+      .then(orders => console.log('orders', orders))
       .catch(err => console.error('Error fetching:', err));
   }
 
