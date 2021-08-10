e1c5a8f0ef6e7127b8d43300bda3e6bb5d17b5c3
Shauna Myers <shaunamyers26@gmail.com>
2021-08-10 17:48:01 +0100

    Change to correct localhost

--- cypress/integration/order_form_spec.js
+++ cypress/integration/order_form_spec.js
@@ -3,6 +3,7 @@ describe('Order Form', () => {
         cy.fixture('orders.json').then((orderData) => {
           cy.intercept('http://localhost:3001/api/v1/orders', orderData)
         })
-        cy.visit('http://localhost:3001/')
+        cy.visit('http://localhost:3000/')
     })
+
 })
