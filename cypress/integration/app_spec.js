0da9afd54847659eac09dbd7988ce7ba81045ee7
Shauna Myers <shaunamyers26@gmail.com>
2021-08-10 17:48:21 +0100

    Write test for past order visibility

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -3,7 +3,7 @@ describe('Order Form', () => {
         cy.fixture('orders.json').then((orderData) => {
           cy.intercept('http://localhost:3001/api/v1/orders', orderData)
         })
-        cy.visit('http://localhost:3001/')
+        cy.visit('http://localhost:3000/')
     })
 
     it('Should be able to visit the page and see a title in the header', () => {
@@ -12,6 +12,13 @@ describe('Order Form', () => {
             .contains('Burrito Builder')
     })
 
+    it.only('Should be able to visit the site and see past orders that have been placed', () => {
+        cy
+            // .get('.order-container')
+            .get('.order')
+            .should('have.length', 3)
+
+    })
     
 
 })
