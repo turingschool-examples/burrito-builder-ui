0817a0c94bb9eff76385d88cd31d444d4c97b5e1
Shauna Myers <shaunamyers26@gmail.com>
2021-08-10 18:52:08 +0100

    Add delete button to order cards

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -11,6 +11,7 @@ const Orders = props => {
             return <li>{ingredient}</li>
           })}
         </ul>
+        <button onClick={handleAddDelete}>Delete</button>
       </div>
     )
   });
