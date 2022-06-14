b81e970eea37ec3136ff797af3e0e1a8d97537c2
Rio Foster <friotious@gmail.com>
2022-06-14 08:59:46 -0700

    Add handleNameChange method

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -25,6 +25,10 @@ class OrderForm extends Component {
     this.setState({name: '', ingredients: []});
   }
 
+  handleNameChange = e => {
+    this.setState({ name: e.target.value })
+  }
+
   render() {
     const possibleIngredients = ['beans', 'steak', 'carnitas', 'sofritas', 'lettuce', 'queso fresco', 'pico de gallo', 'hot sauce', 'guacamole', 'jalapenos', 'cilantro', 'sour cream'];
     const ingredientButtons = possibleIngredients.map(ingredient => {
