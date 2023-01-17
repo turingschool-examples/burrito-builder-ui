aadb091b261c214ce89e7bda2db4f8de4770579d
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2023-01-17 12:22:07 -0700

    Complete 3 of 3 user stories

--- cypress/e2e/burritoBuiler-spec.cy.js
+++ cypress/e2e/burritoBuiler-spec.cy.js
@@ -1,57 +1,72 @@
 describe('Visiting the Burrito Builder page', () => {
   beforeEach(() => {
-    cy.intercept('http://localhost:3001/api/v1/orders',{
-      method: "GET",
-      fixture: "../fixtures/getRequest.json"
-    }).as("GET Request Intercept")
+    cy.intercept('http://localhost:3001/api/v1/orders', {
+      method: 'GET',
+      fixture: '../fixtures/getRequest.json',
+    }).as('GET Request Intercept');
 
-    cy.visit('http://localhost:3000')
-  })
+    cy.visit('http://localhost:3000');
+  });
   it('When a user visits the page, they can view the page title and the existing orders', () => {
-    cy.get('h1').contains("Burrito Builder");
+    cy.get('h1').contains('Burrito Builder');
 
-    cy.get('section > :nth-child(1)').should("contain", "Valentino")
-    .and("contain", "beans")
-    .and("contain", "lettuce")
-    .and("contain", "carnitas")
-    .and("contain", "queso fresco")
-    .and("contain", "jalapeno")
+    cy.get('section > :nth-child(1)')
+      .should('contain', 'Valentino')
+      .and('contain', 'beans')
+      .and('contain', 'lettuce')
+      .and('contain', 'carnitas')
+      .and('contain', 'queso fresco')
+      .and('contain', 'jalapeno');
 
-    cy.get('section > :nth-child(2)').should("contain", "Bambounou")
-    .and("contain", "steak")
-    .and("contain", "pico de gallo")
-    .and("contain", "lettuce")
-    .and("contain", "carnitas")
-    .and("contain", "queso fresco")
-    .and("contain", "jalapeno")
+    cy.get('section > :nth-child(2)')
+      .should('contain', 'Bambounou')
+      .and('contain', 'steak')
+      .and('contain', 'pico de gallo')
+      .and('contain', 'lettuce')
+      .and('contain', 'carnitas')
+      .and('contain', 'queso fresco')
+      .and('contain', 'jalapeno');
 
-    cy.get('section > :nth-child(3)').should("contain", "Sasha")
-    .and("contain", "sofritas")
-    .and("contain", "beans")
-    .and("contain", "sour cream")
-    .and("contain", "carnitas")
-    .and("contain", "queso fresco")
-  })
+    cy.get('section > :nth-child(3)')
+      .should('contain', 'Sasha')
+      .and('contain', 'sofritas')
+      .and('contain', 'beans')
+      .and('contain', 'sour cream')
+      .and('contain', 'carnitas')
+      .and('contain', 'queso fresco');
+  });
   it('When a user visits the page, they can view the Form with the proper inputs', () => {
-    cy.get('form').get('input[name="name"]').type("text")
+    cy.get('form').get('input[name="name"]').type('text');
 
-    cy.get('form').get("button").contains("beans")
-    cy.get('form').get("button").contains("steak")
-    cy.get('form').get("button").contains("carnitas")
-    cy.get('form').get("button").contains("sofritas")
-    cy.get('form').get("button").contains("lettuce")
-    cy.get('form').get("button").contains("queso fresco")
-    cy.get('form').get("button").contains("pico de gallo")
-    cy.get('form').get("button").contains("hot sauce")
-    cy.get('form').get("button").contains("guacamole")
-    cy.get('form').get("button").contains("jalapenos")
-    cy.get('form').get("button").contains("cilantro")
-    cy.get('form').get("button").contains("sour cream")
-    
-    cy.get('form').get("button").contains("Submit Order")
-  })
+    cy.get('form').get('button').contains('beans');
+    cy.get('form').get('button').contains('steak');
+    cy.get('form').get('button').contains('carnitas');
+    cy.get('form').get('button').contains('sofritas');
+    cy.get('form').get('button').contains('lettuce');
+    cy.get('form').get('button').contains('queso fresco');
+    cy.get('form').get('button').contains('pico de gallo');
+    cy.get('form').get('button').contains('hot sauce');
+    cy.get('form').get('button').contains('guacamole');
+    cy.get('form').get('button').contains('jalapenos');
+    cy.get('form').get('button').contains('cilantro');
+    cy.get('form').get('button').contains('sour cream');
 
-  it('When a user fills out the form, the information is reflected in the input field\'s value', () => {
-    
-  })
-})
+    cy.get('form').get('button').contains('Submit Order');
+  });
+
+  it("When a user fills out the form, the information is reflected in the input field's value", () => {
+    cy.get('form').get('input[name="name"]').type('text');
+    cy.get('form').get('input[name="name"]').should('have.value', 'text');
+
+    cy.get('form').get('button[name="beans"]').click();
+    cy.get('form').get('p').should('contain', 'Order: beans');
+    cy.get('form').get('button[name="steak"]').click();
+    cy.get('form').get('p').should('contain', 'Order: beans, steak');
+    cy.get('form').get('button[name="carnitas"]').click();
+    cy.get('form').get('p').should('contain', 'Order: beans, steak, carnitas');
+    cy.get('form').get('button[name="sofritas"]').click();
+    cy.get('form').get('p').should('contain', 'Order: beans, steak, carnitas, sofritas');
+    cy.get('form').get('button[name="lettuce"]').click();
+    cy.get('form').get('p').should('contain', 'Order: beans, steak, carnitas, sofritas, lettuce');
+  });
+});
