6de565891f5b7b6f2126391f3bba6fdcbfdda0d1
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2023-01-17 09:28:24 -0700

    Add error handling to api fetch calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,9 @@
 export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
-      .then(response => response.json())
-}
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Unable to retrieve burritos');
+    }
+    return response.json();
+  });
+};
