4477634f218d62af9b48658c4fa08972da1a4e3d
Justen Anderson <justenanderson@gmail.com>
2023-01-18 11:50:43 -0700

    Fix issue with adding empty ingredients to orders.

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -1,71 +1,89 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class OrderForm extends Component {
   constructor(props) {
-    super();
-    this.props = props;
+    super()
+    this.props = props
     this.state = {
       name: '',
-      ingredients: []
-    };
+      ingredients: [],
+    }
   }
 
   handleNameChange = (e) => {
     this.setState({ ...this.state, name: e.target.value })
-  } 
-
-  handleIngredientChange = (e) => {
-    e.preventDefault();
-    this.setState(({...this.state, ingredients: [...this.state.ingredients, e.target.value]}))
   }
 
-  handleSubmit = e => {
-    e.preventDefault();
+  handleIngredientChange = (e) => {
+    e.preventDefault()
+    this.setState({
+      ...this.state,
+      ingredients: [...this.state.ingredients, e.target.value],
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
     const newOrder = {
       name: this.state.name,
-      ingredients: this.state.ingredients
+      ingredients: this.state.ingredients,
     }
-    // I think I just need to add .length to my ingredients.
-    if(this.state.name && this.state.ingredients) {
+    if (this.state.name && this.state.ingredients.length) {
       this.props.addOrder(newOrder)
-      this.clearInputs();}
+      this.clearInputs()
+    }
   }
 
   clearInputs = () => {
-    this.setState({name: '', ingredients: []});
+    this.setState({ name: '', ingredients: [] })
   }
 
   render() {
-    const possibleIngredients = ['beans', 'steak', 'carnitas', 'sofritas', 'lettuce', 'queso fresco', 'pico de gallo', 'hot sauce', 'guacamole', 'jalapenos', 'cilantro', 'sour cream'];
-    const ingredientButtons = possibleIngredients.map(ingredient => {
+    const possibleIngredients = [
+      'beans',
+      'steak',
+      'carnitas',
+      'sofritas',
+      'lettuce',
+      'queso fresco',
+      'pico de gallo',
+      'hot sauce',
+      'guacamole',
+      'jalapenos',
+      'cilantro',
+      'sour cream',
+    ]
+    const ingredientButtons = possibleIngredients.map((ingredient) => {
       return (
-        <button value={ingredient} key={ingredient} name={ingredient} onClick={e => this.handleIngredientChange(e)}>
+        <button
+          value={ingredient}
+          key={ingredient}
+          name={ingredient}
+          onClick={(e) => this.handleIngredientChange(e)}
+        >
           {ingredient}
         </button>
       )
-    });
+    })
 
     return (
       <form>
         <input
-          type='text'
-          placeholder='Name'
-          name='name'
+          type="text"
+          placeholder="Name"
+          name="name"
           value={this.state.name}
-          onChange={e => this.handleNameChange(e)}
+          onChange={(e) => this.handleNameChange(e)}
         />
 
-        { ingredientButtons }
+        {ingredientButtons}
 
-        <p>Order: { this.state.ingredients.join(', ') || 'Nothing selected' }</p>
+        <p>Order: {this.state.ingredients.join(', ') || 'Nothing selected'}</p>
 
-        <button onClick={e => this.handleSubmit(e)}>
-          Submit Order
-        </button>
+        <button onClick={(e) => this.handleSubmit(e)}>Submit Order</button>
       </form>
     )
   }
 }
 
-export default OrderForm;
-
+export default OrderForm
