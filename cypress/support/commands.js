9d17597a8c961a9577f3394b68d599057dfe95bf
Kirk Hauck <kirkihauck@gmail.com>
2023-04-24 11:53:50 -0600

    test: Visit home page and test elements

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,15 @@
+// Cypress.Commands.add('visitPage', () => {
+//   cy.intercept('http://localhost:3001/api/v1/orders', {
+//     fixture: 'mock_orders',
+//   })
+//   .visit('http://localhost:3000')
+//  });
+
 Cypress.Commands.add('visitPage', () => {
   cy.intercept('http://localhost:3001/api/v1/orders', {
     fixture: 'mock_orders',
   })
+  .as('mockedOrders')
   .visit('http://localhost:3000')
+  .wait('@mockedOrders');
  });
