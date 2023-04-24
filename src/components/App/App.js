bfa73bf134cd6b03e5f393641633e1980a4431db
Kirk Hauck <kirkihauck@gmail.com>
2023-04-24 09:20:41 -0600

    feat: Display data with commented out order

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,31 +1,28 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import {getOrders} from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
-class App extends Component {
-  constructor(props) {
-    super();
-  }
+const App = () => {
+  const [orders, setOrders] = useState()
 
-  componentDidMount() {
+  useEffect(() => {
     getOrders()
+      .then(data => setOrders(data))
       .catch(err => console.error('Error fetching:', err));
-  }
+  }, []);
 
-  render() {
-    return (
-      <main className="App">
-        <header>
-          <h1>Burrito Builder</h1>
-          <OrderForm />
-        </header>
+  return (
+    <main className="App">
+      <header>
+        <h1>Burrito Builder</h1>
+        <OrderForm />
+      </header>
 
-        <Orders orders={this.state.orders}/>
-      </main>
-    );
-  }
+      <Orders orders={orders}/>
+    </main>
+  );
 }
 
 
--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -2,22 +2,23 @@ import React from 'react';
 import './Orders.css';
 
 const Orders = props => {
-  const orderEls = props.orders.map(order => {
-    return (
-      <div className="order">
-        <h3>{order.name}</h3>
-        <ul className="ingredient-list">
-          {order.ingredients.map(ingredient => {
-            return <li>{ingredient}</li>
-          })}
-        </ul>
-      </div>
-    )
-  });
+  // const orderEls = props.orders.map(order => {
+  //   return (
+  //     <div className="order">
+  //       <h3>{order.name}</h3>
+  //       <ul className="ingredient-list">
+  //         {order.ingredients.map(ingredient => {
+  //           return <li>{ingredient}</li>
+  //         })}
+  //       </ul>
+  //     </div>
+  //   )
+  // });
 
   return (
     <section>
-      { orderEls.length ? orderEls : <p>No orders yet!</p> }
+      {/* { orderEls.length ? orderEls : <p>No orders yet!</p> } */}
+      <p>Hello?</p>
     </section>
   )
 }
