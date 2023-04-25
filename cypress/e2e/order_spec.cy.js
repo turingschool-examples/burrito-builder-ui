3256cd83b830ed9422a5e874b5f4caccf50aa61e
Kirk Hauck <kirkihauck@gmail.com>
2023-04-25 09:43:06 -0600

    feat: Test ordering burrito flow

---
+++ cypress/e2e/order_spec.cy.js
@@ -0,0 +1,42 @@
+describe('post new order', () => {
+  beforeEach('setup intercepts and stub page visit', () => {
+    cy.interceptGetOrders();
+    cy.interceptPostOrders();
+    cy.visit('http://localhost:3000');
+    cy.wait('@getInitialOrders');
+  });
+
+  it('should let the user fill out the form and select ingredients', () => {
+    cy.get('input').type('Kirk');
+    
+    cy.get('input')
+      .should('have.value', 'Kirk');
+
+    cy.get('button').contains('steak').click();
+
+    cy.get('button').contains('queso fresco').click();
+  });
+
+  it('should show the user their order, let them submit it, and see it on screen', () => {
+    cy.interceptGetNewOrders();
+    
+    cy.get('input').type('Kirk');
+    cy.get('button').contains('steak').click();
+    cy.get('button').contains('queso fresco').click();
+
+    cy.get('form > p')
+      .contains('Order: steak, queso fresco');
+
+    cy.get('button').contains('Submit Order').click();
+
+    cy.get('.order')
+      .should('have.length', 3);
+    
+    cy.get('.order').last()
+    .contains('h3', 'Kirk')
+    .get('.order').last().find('.ingredient-list').contains('steak')
+    .get('.order').last().find('.ingredient-list').contains('queso fresco');
+  });
+});
+
+// I was unable to finish the rest of the tests.
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -6,8 +6,16 @@ Cypress.Commands.add('interceptGetOrders', () => {
   .as('getInitialOrders')
  });
 
+ Cypress.Commands.add('interceptGetNewOrders', () => {
+  cy.intercept('http://localhost:3001/api/v1/orders', {
+    statusCode: 200,
+    fixture: 'after_post_orders',
+  })
+  .as('getNewOrders')
+ });
+
  Cypress.Commands.add('interceptPostOrders', () => {
-  cy.intercept('POST', '<api url>', {
+  cy.intercept('POST', 'http://localhost:3001/api/v1/orders', {
     statusCode: 200,
     headers: {
       'content-type': 'application/json'
