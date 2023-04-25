b6ca8917eea1e7ecabf931f65e2aa479198bbe6b
Kirk Hauck <kirkihauck@gmail.com>
2023-04-25 10:01:35 -0600

    feat: Test user trying to submit with just an ingredient

--- cypress/e2e/sad_paths_spec.cy.js
+++ cypress/e2e/sad_paths_spec.cy.js
@@ -6,12 +6,32 @@ describe('Sad Path: User Tries to Submit Order without All Requirements', () => 
   });
   
   it('should not allow the user to submit an order without a name or ingredients', () => {
-    cy.intercept('GET', 'http://localhost:3001/api/v1/orders')
-    
     cy.get('section')
       .find('.order').should('have.length', 2);
     
     cy.get('button').contains('Submit Order').click();
+
+    cy.get('section')
+      .find('.order').should('have.length', 2);
+  });
+
+  it('should not allow the user to submit an order with just a name', () => {
+    cy.get('section')
+      .find('.order').should('have.length', 2);
+    
+    cy.get('input').type('Kirk');
+
+    cy.get('button').contains('Submit Order').click();
+
+    cy.get('section')
+      .find('.order').should('have.length', 2);
+  });
+
+  it('should not allow the user to submit an order with just an ingredient', () => {
+    cy.get('section')
+      .find('.order').should('have.length', 2);
+
+    cy.get('button').first().click();
 
     cy.get('section')
       .find('.order').should('have.length', 2);
