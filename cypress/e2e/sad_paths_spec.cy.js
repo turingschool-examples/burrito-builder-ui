603fbb4cdf8804397ec53922e6ece84a91b6fe51
Kirk Hauck <kirkihauck@gmail.com>
2023-04-25 10:27:08 -0600

    feat: Add sad path message prompting user to complete form to Cypress testing

--- cypress/e2e/sad_paths_spec.cy.js
+++ cypress/e2e/sad_paths_spec.cy.js
@@ -12,30 +12,35 @@ describe('Sad Path: User Tries to Submit Order without All Requirements', () => 
       .find('.order').should('have.length', 2);
     
     cy.get('button').contains('Submit Order').click();
-
+    
     cy.get('section')
-      .find('.order').should('have.length', 2);
+    .find('.order').should('have.length', 2)
+    .get('p').first().contains('Please enter a name and select at least one ingredient.')
   });
-
+  
   it('should not allow the user to submit an order with just a name', () => {
     cy.get('section')
-      .find('.order').should('have.length', 2);
+    .find('.order').should('have.length', 2);
     
     cy.get('input').type('Kirk');
-
+    
     cy.get('button').contains('Submit Order').click();
-
+    
     cy.get('section')
-      .find('.order').should('have.length', 2);
+    .find('.order').should('have.length', 2)
+    .get('p').first().contains('Please enter a name and select at least one ingredient.')
   });
-
+  
   it('should not allow the user to submit an order with just an ingredient', () => {
     cy.get('section')
-      .find('.order').should('have.length', 2);
-
+    .find('.order').should('have.length', 2);
+    
     cy.get('button').first().click();
-
+    
+    cy.get('button').contains('Submit Order').click();
+    
     cy.get('section')
-      .find('.order').should('have.length', 2);
+    .find('.order').should('have.length', 2)
+    .get('p').first().contains('Please enter a name and select at least one ingredient.')
   });
 })
