78163548812f82d28c530f248253e1ed16267a52
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-11-13 12:40:55 -0500

    Fix ingredient buttons

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 
 function OrderForm(props) {
+
+  console.log("props", props)
   const [name, setName] = useState("");
   const [ingredients, setIngredients] = useState([]);
 
@@ -29,12 +31,24 @@ function OrderForm(props) {
     "sour cream",
   ];
 
+
+  // when i click on lettuce button, i want the value of that button (a string of lettuce) to be added to the state array (ingredients). 
+
+  // code: grab ahold of the button with event.target
+  // add event.target to ingredients array with spread operator
+
+  // do i need event.preventDefault()?
+  
   const ingredientButtons = possibleIngredients.map((ingredient) => {
     return (
       <button
         key={ingredient}
         name={ingredient}
-        onClick={(e) => setIngredients(e.target.value)}
+        onClick={(e) => {
+          console.log("event!", e.target.name)
+          e.preventDefault()
+          setIngredients([...ingredients, e.target.name])
+        }}
       >
         {ingredient}
       </button>
@@ -61,3 +75,6 @@ function OrderForm(props) {
 }
 
 export default OrderForm;
+
+// ingredients.join(",") takes the ingredients array stored in state? and separates each 
+// ingredient at the comma and returns a string.
